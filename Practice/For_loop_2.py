b6f45a19e7eb7bdbb82386b831970f6912e79138
SayanCispl <sayan.koley@codeclouds.co.in>
2026-02-23 12:41:07 +0530

    23-02-2026

---
+++ Practice/For_loop_2.py
@@ -0,0 +1,3 @@
+for k in range (1, 10, 2):
+    #In this case, the loop will start from 1 and will increment by 2 until it reaches 10 (exclusive). So, it will print the odd numbers from 1 to 9.
+    print(k)
